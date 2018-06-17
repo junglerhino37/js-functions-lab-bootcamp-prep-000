f1c48affcec58f3687a0ec1f8fa7d13874186667
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-17 21:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysto(you){
+  return `Happy holidays, ${you}!`
+}
+
+function happyHolidaysto(holiday,you){
+  return `Happy holidays, ${you}!`
+}
